fe1cc9d703bf8775eeeef20d8e943f99fdd45ede
meshtag <prathameshtagore@gmail.com>
2021-12-13 02:39:34 +0530

    Second check

--- tests/UnitTests/ImageProcessing/DIPCorr2D.cpp
+++ tests/UnitTests/ImageProcessing/DIPCorr2D.cpp
@@ -44,7 +44,8 @@ bool equalImages(const Mat &img1, const Mat &img2)
 Mat inputImage = imread("../../benchmarks/ImageProcessing/Images/YuTu.png",
                              IMREAD_GRAYSCALE);
 
-void testKernel(unsigned int kernelRows, unsigned int kernelCols, float* kernelArray)
+void testKernelImpl(unsigned int kernelRows, unsigned int kernelCols, float* kernelArray, 
+                unsigned int x, unsigned int y)
 {
   // Define allocated, sizes, and strides.
   intptr_t sizesInput[2] = {inputImage.rows, inputImage.cols};
@@ -56,26 +57,28 @@ void testKernel(unsigned int kernelRows, unsigned int kernelCols, float* kernelA
   MemRef<float, 2> kernel(kernelArray, sizesKernel);
   MemRef<float, 2> output(sizesOutput);
 
-  // Define a cv::Mat with the output of the corr_2d.
-  Mat dipOutput(inputImage.rows, inputImage.cols, CV_32FC1, output.getData());
   Mat kernel1 = Mat(3, 3, CV_32FC1, kernelArray);
   Mat opencvOutput;
 
-  for (unsigned int x = 0; x < kernelRows; ++x)
-  {
-    for (unsigned int y = 0; y < kernelCols; ++y)
-    {
-      _mlir_ciface_corr_2d(input, kernel, output, x, y, 1);
-      filter2D(inputImage, opencvOutput, CV_8UC1, kernel1, cv::Point(x, y), 0.0,
-           cv::BORDER_REPLICATE);
+  _mlir_ciface_corr_2d(input, kernel, output, x, y, 1);
+  filter2D(inputImage, opencvOutput, CV_8UC1, kernel1, cv::Point(x, y), 0.0,
+        cv::BORDER_REPLICATE);
 
-      if (!equalImages(dipOutput, opencvOutput))
-      {
-        std::cout << "Different images produced by OpenCV and DIP for kernel " << kernel1 << "\n";
-        return;
-      }
-    }
+  // Define a cv::Mat with the output of the corr_2d.
+  Mat dipOutput(inputImage.rows, inputImage.cols, CV_32FC1, output.getData());
+
+  if (!equalImages(dipOutput, opencvOutput))
+  {
+    std::cout << "Different images produced by OpenCV and DIP for kernel " << kernel1 << "\n";
+    return;
   }
+}
+
+void testKernel(unsigned int kernelRows, unsigned int kernelCols, float* kernelArray)
+{
+  for (unsigned int x = 0; x < kernelRows; ++x)
+    for (unsigned int y = 0; y < kernelCols; ++y)
+      testKernelImpl(kernelRows, kernelCols, kernelArray, x, y);
 }
 
 int main()
